76894d69f4debc7bd06b11e95d32761a0696cbf2
Alanvega2002 <ala.vegac@duocuc.cl>
2023-06-15 20:21:00 -0400

    holaaaaaaaaaaa

---
+++ 15del06.py
@@ -0,0 +1,18 @@
+menu_activo= True
+opcion_seleccionada=0
+while menu_activo:
+    print("ingrese una de las siguientes opciones: ")
+    print("1.- Calcular IVA")
+    print("2.- Descuento del producto")
+    print("3.- Calcular IMC")
+    print("4.- Salir")
+    opcion_seleccionada=int(input("escriba su opcion"))
+    if opcion_seleccionada ==1:
+        print("selecciono la opcion: ",opcion_seleccionada) 
+        
+    if opcion_seleccionada ==2:
+        print("selecciono la opcion: ",opcion_seleccionada) 
+    if opcion_seleccionada ==3:
+        print("selecciono la opcion: ",opcion_seleccionada) 
+    if opcion_seleccionada ==4:
+        print("selecciono la opcion: ",opcion_seleccionada) 
